b793981d152a85d1bb28a68906df14d1698cf46b
codhuck <rslvklmc@gmail.com>
2025-06-25 09:33:45 +0300

    add low_pass_filter

--- include/eureka_inverse/main.hpp
+++ include/eureka_inverse/main.hpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <cmath>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
@@ -12,4 +13,7 @@ private:
 
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher;
+    mutable double filtered_angular_z;
+    const double alpha;
 };
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include "eureka_inverse/main.hpp"
 
-CmdVelProcessor::CmdVelProcessor() : Node("inverse_cmd_vel")
+CmdVelProcessor::CmdVelProcessor() 
+: Node("inverse_cmd_vel"), 
+  filtered_angular_z(0.0), 
+  alpha(0.2)
 {
     subscription = this->create_subscription<geometry_msgs::msg::Twist>(
-        "/custom_cmd_vel", 10, std::bind(&CmdVelProcessor::topic_callback, this, std::placeholders::_1));
+        "/custom_cmd_vel", 
+        10, 
+        std::bind(&CmdVelProcessor::topic_callback, this, std::placeholders::_1));
+    
     publisher = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
 }
 
@@ -13,21 +19,16 @@ void CmdVelProcessor::topic_callback(const geometry_msgs::msg::Twist::SharedPtr 
     modified_msg.linear.x = msg->linear.x;
     modified_msg.linear.y = msg->linear.y;
     modified_msg.linear.z = msg->linear.z;
-
-    if (msg->linear.x < 0.0) 
-    {
-        modified_msg.angular.z = -msg->angular.z;
-    } else 
-    {
-        modified_msg.angular.z = msg->angular.z;
-    }
+    filtered_angular_z = alpha * (msg->angular.z/msg->linear.x) + (1.0 - alpha) * filtered_angular_z;
+    modified_msg.angular.z = filtered_angular_z;
     publisher->publish(modified_msg);
 }
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CmdVelProcessor>());
+    auto node = std::make_shared<CmdVelProcessor>();
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
